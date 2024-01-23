9c5194df2145981665e2286c54340e1d96adfcd4
artemRusachenko <artemrusachenko@gmail.com>
2024-01-23 02:23:27 +0200

    End of section 2

---
+++ Persistence/Migrations/20240122232303_UpdateNameOfTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNameOfTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activity",
+                table: "Activity");
+
+            migrationBuilder.RenameTable(
+                name: "Activity",
+                newName: "Activities");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Activities",
+                table: "Activities");
+
+            migrationBuilder.RenameTable(
+                name: "Activities",
+                newName: "Activity");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Activity",
+                table: "Activity",
+                column: "Id");
+        }
+    }
+}
