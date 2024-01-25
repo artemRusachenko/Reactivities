b2945bfaa6ae30557fe368ea04c238062eff24f3
artemRusachenko <artemrusachenko@gmail.com>
2024-01-25 02:35:10 +0200

    End of section 4

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Comand: IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler: IRequestHandler<Comand>{
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+            _context = context;
+                
+            }
+
+            public async Task Handle(Comand request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Add(request.Activity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
