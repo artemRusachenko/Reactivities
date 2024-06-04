7c81feb79c8b653095e861f2a8f3c1b77dfc454a
artemRusachenko <artemrusachenko@gmail.com>
2024-06-04 16:55:19 +0300

    Try deploying

--- API/Program.cs
+++ API/Program.cs
@@ -28,7 +28,7 @@ app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
-app.UseCspReportOnly(opt => opt
+app.UseCsp(opt => opt
     .BlockAllMixedContent()
     .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com", "sha256-DpOoqibK/BsYhobWHnU38Pyzt5SjDZuR/mFsAiVN7kk="))
     .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
